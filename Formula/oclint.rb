c4ac3db7e022aad798402732af5cf36cf4dfe928
Misaka <misakatao@gmail.com>
2024-04-26 11:25:06 +0800

    add cask oclint

--- Formula/oclint.rb
+++ Formula/oclint.rb
@@ -1,19 +1,28 @@
-cask "oclint" do
+class Oclint < Formula
   arch arm: "arm64", intel: "x86_64"
+
+  desc "OCLint static code analysis tool for C, C++, and Objective-C"
+  homepage "https://oclint.org/"
 
   version "22.02"
   sha256 "6f102a568af3a4344f9658b5f4bdf3d599a851456287bf7a1fae447891f7368c"
 
   url "https://github.com/oclint/oclint/releases/download/v#{version}/oclint-#{version}-llvm-13.0.1-#{arch}-darwin-macos-12.2-xcode-13.2.tar.gz",
       verified: "github.com/oclint/oclint/"
-  name "OCLint"
-  desc "Static source code analysis tool"
-  homepage "https://oclint.org/"
+  head "https://github.com/oclint/oclint.git"
 
-  binary "oclint-#{version}/bin/oclint-json-compilation-database"
-  binary "oclint-#{version}/bin/oclint-xcodebuild"
-  binary "oclint-#{version}/bin/oclint"
-  binary "oclint-#{version}/include/c++/v1", target: "#{HOMEBREW_PREFIX}/include/c++/v1"
-  binary "oclint-#{version}/lib/clang", target: "#{HOMEBREW_PREFIX}/lib/clang"
-  binary "oclint-#{version}/lib/oclint", target: "#{HOMEBREW_PREFIX}/lib/oclint"
+  def install
+    clang_version = '11.0.0'
+
+    include.install Dir['include/c++'] unless File.directory? "#{include}/c++"
+    "#{include}/c++".install Dir['include/c++/v1'] unless File.directory? "#{include}/c++/v1"
+    lib.install Dir['lib/clang'] unless File.directory? "#{lib}/clang"
+    "#{lib}/clang".install Dir['lib/clang/#{clang_version}'] unless File.directory? "#{lib}/clang/#{clang_version}"
+    lib.install Dir['lib/oclint']
+    bin.install Dir['bin/*']
+  end
+
+  test do
+    system "#{bin}/oclint", "-version"
+  end
 end
