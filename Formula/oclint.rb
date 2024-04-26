aa2814c35f0481d081a8847fab5126c0d6d6d31a
Misaka <misakatao@gmail.com>
2024-04-26 11:20:36 +0800

    add cask oclint

---
+++ Formula/oclint.rb
@@ -0,0 +1,19 @@
+cask "oclint" do
+  arch arm: "arm64", intel: "x86_64"
+
+  version "22.02"
+  sha256 "6f102a568af3a4344f9658b5f4bdf3d599a851456287bf7a1fae447891f7368c"
+
+  url "https://github.com/oclint/oclint/releases/download/v#{version}/oclint-#{version}-llvm-13.0.1-#{arch}-darwin-macos-12.2-xcode-13.2.tar.gz",
+      verified: "github.com/oclint/oclint/"
+  name "OCLint"
+  desc "Static source code analysis tool"
+  homepage "https://oclint.org/"
+
+  binary "oclint-#{version}/bin/oclint-json-compilation-database"
+  binary "oclint-#{version}/bin/oclint-xcodebuild"
+  binary "oclint-#{version}/bin/oclint"
+  binary "oclint-#{version}/include/c++/v1", target: "#{HOMEBREW_PREFIX}/include/c++/v1"
+  binary "oclint-#{version}/lib/clang", target: "#{HOMEBREW_PREFIX}/lib/clang"
+  binary "oclint-#{version}/lib/oclint", target: "#{HOMEBREW_PREFIX}/lib/oclint"
+end
